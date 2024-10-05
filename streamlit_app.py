fd04b94bafe1e6e694a422797ac384b61c6299c6
AlexaLeshaya <125724078+AlexaLeshaya@users.noreply.github.com>
2024-10-05 19:11:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
 
 with st.sidebar:
-   st.header('Inout Features')
-  island = st.selectbox('Island', {'Biscoe', 'Dream', 'Torgerson'})
+  st.header('Inout Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
   bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
   flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
