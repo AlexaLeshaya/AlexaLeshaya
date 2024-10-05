b6654641bf5fa2ae8b5430ba1f6ff0f4a4c6f8d4
AlexaLeshaya <125724078+AlexaLeshaya@users.noreply.github.com>
2024-10-05 18:48:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('👀 AlexaLeshaya')
 
 st.write('Hello world!')
+
+with st.expander('Initial data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
