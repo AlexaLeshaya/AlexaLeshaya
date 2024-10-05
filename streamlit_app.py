fdecaf7d60a9d830b283e93cb9ac50ba6566f9db
AlexaLeshaya <125724078+AlexaLeshaya@users.noreply.github.com>
2024-10-05 18:59:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,18 @@ import pandas as pd
 
 st.title('👀 AlexaLeshaya')
 
-st.write('Hello world!')
-
 with st.expander('Initial data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
-  st.write('**X**')
-  X_raw = df.drop('species', axis = 1)
-  X_raw
   
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
   st.write('**y**')
   y_raw = df.species
   y_raw
 
 with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x = 'bill_lenght_nm', y = 'body_mass_g', color = 'species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
   
