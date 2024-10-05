19ac8d5d3209599b05ba82d00a6f66d094004899
AlexaLeshaya <125724078+AlexaLeshaya@users.noreply.github.com>
2024-10-05 19:36:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensamble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
 st.title('👀 AlexaLeshaya')
